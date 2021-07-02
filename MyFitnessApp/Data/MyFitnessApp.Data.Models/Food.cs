c2c21b6c7e6621461343ddf7a56c2a8f271f952b
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-07-02 11:52:01 +0300

    Create Food.cs

---
+++ MyFitnessApp/Data/MyFitnessApp.Data.Models/Food.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFitnessApp.Data.Models
+{
+    public class Food
+    {
+        public string Name { get; set; }
+
+        public double Carbs { get; set; }
+    }
+}
