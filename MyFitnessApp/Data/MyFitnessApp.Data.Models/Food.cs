fab515cd89326658693de71da5b372f0e4210223
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-07-02 11:40:54 +0300

    Update Food.cs

--- MyFitnessApp/Data/MyFitnessApp.Data.Models/Food.cs
+++ MyFitnessApp/Data/MyFitnessApp.Data.Models/Food.cs
@@ -7,9 +7,5 @@ namespace MyFitnessApp.Data.Models
     public class Food
     {
         public string Test1 { get; set; }
-
-        public int Test2 { get; set; }
-
-        public bool Test3 { get; set; }
     }
 }
