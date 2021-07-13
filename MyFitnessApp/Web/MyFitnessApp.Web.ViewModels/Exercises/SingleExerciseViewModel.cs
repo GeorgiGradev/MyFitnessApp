127140768130129cd16045d9445d2062d34e5770
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-07-13 17:56:04 +0300

    Update SingleExerciseViewModel.cs

--- MyFitnessApp/Web/MyFitnessApp.Web.ViewModels/Exercises/SingleExerciseViewModel.cs
+++ MyFitnessApp/Web/MyFitnessApp.Web.ViewModels/Exercises/SingleExerciseViewModel.cs
@@ -6,6 +6,8 @@
     // Визуализира едно упражнение
     public class SingleExerciseViewModel : IMapFrom<Exercise> // , IHaveCustomMappings
     {
+        public int Id { get; set; }
+
         public string Name { get; set; }
 
         public string Description { get; set; }
