cbcd74368b7a635a7133854a820f873041712aef
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-08-05 12:04:27 +0300

    Implement Bootstrap font awesome for Admin area

--- MyFitnessApp/Web/MyFitnessApp.Web/wwwroot/js/site.js
+++ MyFitnessApp/Web/MyFitnessApp.Web/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+/*05.08.2021*/
+/*Bootstrap 4 Swing Counter with font awesome*/
+/*https://bbbootstrap.com/snippets/swing-counter-font-awesome-73778739*/
+$(document).ready(function () {
+
+    $('.counter').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+});
