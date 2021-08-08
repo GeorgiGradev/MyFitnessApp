b9c8a7412c6ef82c6833ad1f65242e6f501bb300
Georgi Gradev <georgi.d.gradev@gmail.com>
2021-08-08 22:27:30 +0300

    Update HomeController.cs

--- MyFitnessApp/Web/MyFitnessApp.Web/Controllers/HomeController.cs
+++ MyFitnessApp/Web/MyFitnessApp.Web/Controllers/HomeController.cs
@@ -3,7 +3,6 @@
     using System.Diagnostics;
 
     using Microsoft.AspNetCore.Mvc;
-    using MyFitnessApp.Web.Filters;
     using MyFitnessApp.Web.ViewModels;
     using MyFitnessApp.Web.ViewModels.Home;
 
